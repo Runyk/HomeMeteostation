f3dfbe7b632f6915bc2763aed8d1a3911ef16228
Igor <igorek1213@list.ru>
2026-02-02 23:21:24 +0300

    file application.properties is update

--- backend/src/main/java/ru/runyk/meteostation_backend/service/TelegramBotService.java
+++ backend/src/main/java/ru/runyk/meteostation_backend/service/TelegramBotService.java
@@ -50,7 +50,7 @@ public class TelegramBotService extends TelegramLongPollingBot {
         }
     }
 
-    @Scheduled(fixedDelay = 120000)
+    @Scheduled(fixedDelay = 60000)
     public void sendNotification() {
         SensorDataDTO data = arduinoService.getLastSensorData();
 
